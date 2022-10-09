7068d87e2abc6d6ca8472d870070826cec92eab2
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-10-09 11:05:30 -0400

    CODE - Added functionality for drawing a random fortune card

--- PiratenKapern/src/main/java/Config.java
+++ PiratenKapern/src/main/java/Config.java
@@ -1,5 +1,16 @@
 public class Config {
     public static final int PORT = 3333;
     public static final int NUM_OF_PLAYERS = 3;
-
+    public static final int NUM_OF_CARDS = 35;
+    public static final int NUM_OF_CHEST_CARDS = 4;
+    public static final int NUM_OF_SORCERESS_CARDS = 4;
+    public static final int NUM_OF_CAPTAIN_CARDS = 4;
+    public static final int NUM_OF_MONKEY_AND_PARROT_CARDS = 4;
+    public static final int NUM_OF_DIAMOND_CARDS = 4;
+    public static final int NUM_OF_COIN_CARDS = 4;
+    public static final int NUM_OF_TWO_SKULLS_CARDS = 2;
+    public static final int NUM_OF_ONE_SKULL_CARDS = 3;
+    public static final int NUM_OF_TWO_SWORDS_CARDS = 2;
+    public static final int NUM_OF_THREE_SWORDS_CARDS = 2;
+    public static final int NUM_OF_FOUR_SWORDS_CARDS = 2;
 }
--- PiratenKapern/src/main/java/Game.java
+++ PiratenKapern/src/main/java/Game.java
@@ -1,2 +1,64 @@
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.Random;
+
 public class Game {
+    private int numCards;
+
+
+
+    private EnumMap<Card, Integer> fortuneCards;
+
+    public enum Card {
+        CHEST, SORCERESS, CAPTAIN, MONKEY_AND_PARROT, DIAMOND, COIN, TWO_SKULLS, ONE_SKULL, TWO_SWORDS, THREE_SWORDS, FOUR_SWORDS;
+    }
+
+    public Game(){
+        numCards = Config.NUM_OF_CARDS;
+        fortuneCards = new EnumMap<Card, Integer>(Card.class);
+        fortuneCards.put(Card.CHEST, Config.NUM_OF_CHEST_CARDS);
+        fortuneCards.put(Card.SORCERESS, Config.NUM_OF_SORCERESS_CARDS);
+        fortuneCards.put(Card.CAPTAIN, Config.NUM_OF_CAPTAIN_CARDS);
+        fortuneCards.put(Card.MONKEY_AND_PARROT, Config.NUM_OF_MONKEY_AND_PARROT_CARDS);
+        fortuneCards.put(Card.DIAMOND, Config.NUM_OF_DIAMOND_CARDS);
+        fortuneCards.put(Card.COIN, Config.NUM_OF_COIN_CARDS);
+        fortuneCards.put(Card.TWO_SKULLS, Config.NUM_OF_TWO_SKULLS_CARDS);
+        fortuneCards.put(Card.ONE_SKULL, Config.NUM_OF_ONE_SKULL_CARDS);
+        fortuneCards.put(Card.TWO_SWORDS, Config.NUM_OF_TWO_SWORDS_CARDS);
+        fortuneCards.put(Card.THREE_SWORDS, Config.NUM_OF_THREE_SWORDS_CARDS);
+        fortuneCards.put(Card.FOUR_SWORDS, Config.NUM_OF_FOUR_SWORDS_CARDS);
+    }
+
+    public int getNumCards() {
+        return numCards;
+    }
+
+    public EnumMap<Card, Integer> getFortuneCards() {
+        return fortuneCards;
+    }
+
+    public Card drawFortuneCard(){
+        Random rand = new Random();
+
+        //Creating an arraylist that will hold remaining fortune cards
+        ArrayList<Card> cards = new ArrayList<>();
+        for(Card card: fortuneCards.keySet()){
+            for(int i = 0; i < fortuneCards.get(card); i++){
+                cards.add(card);
+            }
+        }
+
+        int max = cards.size()-1;
+        int min = 0;
+
+        //Generating a random card
+        int index = rand.nextInt((max-min) + 1) + min;
+        Card card = cards.get(index);
+
+        fortuneCards.put(card, fortuneCards.get(card)-1);
+
+        numCards -= 1;
+        return card;
+    }
+
 }
