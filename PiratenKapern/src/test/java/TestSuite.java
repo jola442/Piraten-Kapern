628034ca459bb6046e11028cc90512ee0a08371a
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-10-12 16:54:28 -0400

    A-TEST - Acceptance Test Part 1

--- PiratenKapern/src/test/java/TestSuite.java
+++ PiratenKapern/src/test/java/TestSuite.java
@@ -2,7 +2,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({GameUnitTest.class})
+@SelectClasses({GameUnitTest.class, AcceptanceTestPart1.class})
 public class TestSuite {
 
 }
